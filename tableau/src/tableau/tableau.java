6746e683928fd898b83762d5762d028b9d58c427
[3steak] <[cyprien.bocquet@gmail.com]>
2023-05-30 11:00:32 +0200

    ajout méthode pour exo sur les tableaux

--- tableau/src/tableau/tableau.java
+++ tableau/src/tableau/tableau.java
@@ -4,13 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class tableau {
-	public static void main(String[] args) {
-	List<String> myList = new ArrayList<String>();
-			    myList.add("Virgile");
-			    myList.add("Oui");
 
-			      System.out.println(myList);
-			    
-			  }
-			}
-	
+	// creation méthode pour print les tableaux
+	public static void printTab(int[] tab) {
+
+		// boucler un tableau
+		for (int number : tab)
+			System.out.println(number);
+	}
+
+	public static void main(String[] args) {
+		List<String> myList = new ArrayList<String>();
+		myList.add("Virgile");
+		myList.add("Oui");
+
+		System.out.println(myList);
+
+		int[] tab = { 1, 2, 3 };
+		printTab(tab);
+	}
+}
